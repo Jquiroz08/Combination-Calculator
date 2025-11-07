9bb7773272f9dfa791de2897f03c2d45d7f4eeb4
Joseph Quiroz <joqu6657@stu.sjcc.edu>
2025-11-06 17:16:38 -0800

    Calculate combinations

--- combination_example.cpp
+++ combination_example.cpp
@@ -31,7 +31,7 @@ int main() {
         return 0;
     }
     // calculate C(n,k) = n! / (k! * (n-k)!)
-    uint16_t c_n_k = factorial(n);
+    uint16_t c_n_k = factorial(n)/((factorial(k)*factorial(n-k)));
 
     // write out results
     cout << "result = " << c_n_k << endl;
