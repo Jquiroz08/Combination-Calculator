37a1c891ee4f443d6969ff4e389d566962e5db07
Joseph Quiroz <joqu6657@stu.sjcc.edu>
2025-11-06 17:08:51 -0800

    Added user input

--- combination_example.cpp
+++ combination_example.cpp
@@ -5,18 +5,30 @@ using namespace std;
 /*
  * *** STUDENTS SHOULD WRITE CODE FOR THIS FUNCTION ***
  */
-uint16_t factorial(const uint16_t x) { return x; }
+uint16_t factorial(const uint16_t x) { 
+
+
+
+
+    return x; }
 
 /*
  * *** STUDENTS SHOULD WRITE CODE FOR THIS FUNCTION ***
  */
 int main() {
     int n;
+    int k;
 
     // get and validate user input
-    cout << "Enter n: ";
+    cout << "Enter set number n: ";
     cin >> n;
+    cout << "Enter subset number k: ";
+    cin >> k;
 
+    if(n < 0 || k < 0){
+        cout<<"Can't calculate combination with negative integers";
+        return 0;
+    }
     // calculate C(n,k) = n! / (k! * (n-k)!)
     uint16_t c_n_k = factorial(n);
 
